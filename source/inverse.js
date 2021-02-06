bb419fab45b0be8f97e6c8d9e061770e3e457424
Suvorov_Nikita <suvnik.2020@gmail.com>
2021-02-07 00:30:51 +0300

    Add check for incorrect data

--- source/inverse.js
+++ source/inverse.js
@@ -1,8 +1,13 @@
 'use strict'
 
+
 const inverse = (array, num = 0) => {
+	if (!Array.isArray(array) || !Number.isInteger(num))
+		throw new TypeError('Incorrect input data');
+
 	let slc1 = array.slice(0, num), 
 		slc2 = array.slice(num);
 
 	return (num >= 0) ? [...slc1, ...slc2.reverse()] : [...slc1.reverse(), ...slc2];
 }
+
