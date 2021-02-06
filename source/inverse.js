839cfdb2910a3258f31ab9572be5109a96d5ba67
Suvorov_Nikita <suvnik.2020@gmail.com>
2021-02-07 01:49:30 +0300

    Fix tests

--- source/inverse.js
+++ source/inverse.js
@@ -32,9 +32,9 @@ const inverse = (array, num = 0) => {
 	if (!Array.isArray(array) || !Number.isInteger(num))
 		throw new TypeError('Incorrect input data');
 
-	let slc1 = array.slice(0, num), 
-		slc2 = array.slice(num);
+	const slice1 = array.slice(0, num); 
+	const slice2 = array.slice(num);
 
-	return (num >= 0) ? [...slc1, ...slc2.reverse()] : [...slc1.reverse(), ...slc2];
+	return (num >= 0) ? [...slice1, ...slice2.reverse()] : [...slice1.reverse(), ...slice2];
 }
 
